fd6692fcef234e6adb53b27ad8d4bc1408411ea7
aldimhernandez <marialdanahz@gmail.com>
2026-06-25 23:51:55 -0300

    fix: corrección 1/4

--- src/entities/Base.java
+++ src/entities/Base.java
@@ -1,32 +1,48 @@
 package entities;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 /**
- * UTN - TUPAD - PROGRAMACIÓN 2 Trabajo Práctico N° -
+ * UTN - TUPAD - PROGRAMACIÓN 2 - Trabajo Práctico Integrador
  *
- * @author María Aldana Hernández Cohorte Agosto 2025 - Comisión: 5 Matrícula 102505
+ * @author María Aldana Hernández - Cohorte Agosto 2025 - Comisión: 5 - Matrícula 102505
  */
 public abstract class Base {
 
-    private static Long contadorId = 1L;
+    private static final Map<Class<?>, Long> CONTADORES = new HashMap<>();
+
+//    private static Long contadorId = 1L;
     private Long id;
     private boolean eliminado;
     private LocalDateTime createdAt;
 
     public Base() {
-        this.id = contadorId;
-        contadorId++;
+        this.id = generarIdPorClase(getClass());
         this.eliminado = false;
         this.createdAt = LocalDateTime.now();
     }
 
-    public Long getId() {
-        return id;
+    /* Corrección examen parcial 2:
+    1. Modificar la clase Base para incluir un constructor que reciba un Long id, 
+    permitiendo a las subclases delegar explícitamente la identidad al padre como se solicitaba.
+     */
+    public Base(Long id) {
+        this.id = id;
+        this.eliminado = false;
+        this.createdAt = LocalDateTime.now();
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    private static Long generarIdPorClase(Class<?> clase) {
+        Long proximoId = CONTADORES.getOrDefault(clase, 0L) + 1;
+        CONTADORES.put(clase, proximoId);
+        return proximoId;
+    }
+
+    public Long getId() {
+        return id;
     }
 
     public boolean isEliminado() {
@@ -41,10 +57,26 @@ public abstract class Base {
         return createdAt;
     }
 
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
+    @Override
+    public abstract String toString();
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getClass(), id);
     }
 
     @Override
-    public abstract String toString();
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Base other = (Base) obj;
+        return Objects.equals(this.id, other.id);
+    }
 }
