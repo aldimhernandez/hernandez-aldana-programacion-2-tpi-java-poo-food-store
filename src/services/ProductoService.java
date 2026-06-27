1fdd0980099578b5591f4c37a99ecc7991f49252
aldimhernandez <marialdanahz@gmail.com>
2026-06-27 20:03:18 -0300

    feat: add services to CRUD logic

---
+++ src/services/ProductoService.java
@@ -0,0 +1,38 @@
+
+package services;
+
+import interfaces.Service;
+
+/**
+ * UTN - TUPAD - PROGRAMACIÓN 2
+ * Trabajo Práctico N° -
+ * @author María Aldana Hernández
+ * Cohorte Agosto 2025 - Comisión: 5
+ * Matrícula 102505
+ */
+public class ProductoService implements Service {
+    
+    @Override
+    public void listar() {
+        //TODO: 
+        System.out.println("ProductoService listar");
+    }
+
+    @Override
+    public void crear() {
+        //TODO: 
+        System.out.println("ProductoService crear");
+    }
+
+    @Override
+    public void editar() {
+        //TODO: 
+        System.out.println("ProductoService editar");
+    }
+
+    @Override
+    public void eliminar() {
+        //TODO: 
+        System.out.println("ProductoService eliminar");
+    }
+}
